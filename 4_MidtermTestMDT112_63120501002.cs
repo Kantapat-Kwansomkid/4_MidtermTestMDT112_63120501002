f07d1d7e18ab779fad74a7717c910524474b9b6a
Kantapat-Kwansomkid <72593812+Kantapat-Kwansomkid@users.noreply.github.com>
2021-04-24 15:30:07 +0700

    Add files via upload

---
+++ 4_MidtermTestMDT112_63120501002.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _4_MidtermTestMDT112_63120501002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Define floats = x, y, dx, dy, step;
+            Input floats = x1, y, x2, y2;
+            dx = x2 - x1;
+            dy = y2 - y1;
+            is abs(dx) >= abs(dy);
+            step = abs(dx);
+            step = abs(dynamic);
+            dx /= step;
+            dy /= step;
+            x = x1;
+            y = y1;
+            Define number i = 1
+            Is i <= step
+            
+            x += dx
+            y += dy
+
+
+            if (is abs(dx) >= abs(dy))
+            {
+                Console.WriteLine("step = abs(dy)");
+            }
+            else
+            {
+                Console.WriteLine("step = abs(dx)");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
